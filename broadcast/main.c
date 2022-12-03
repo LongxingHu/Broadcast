530e964113819680824092f9beca22f1358e2211
Longxing Hu <59757519+LongxingHu@users.noreply.github.com>
2022-12-03 20:16:38 +0800

    Add files via upload

---
+++ broadcast/main.c
@@ -0,0 +1,76 @@
+#include "headers.h"
+#include "base.h"
+#include "ether.h"
+#include "log.h"
+
+#include <sys/types.h>
+#include <ifaddrs.h>
+
+void handle_packet(iface_info_t *iface, char *packet, int len)
+{
+	broadcast_packet(iface, packet, len);
+	free(packet);
+}
+
+void ustack_run()
+{
+	struct sockaddr_ll addr;			//物理地址
+	socklen_t addr_len = sizeof(addr);
+	char buf[ETH_FRAME_LEN];			//以太网帧的最大长度为1514B
+	int len;
+
+	while (1) {
+		int ready = poll(instance->fds, instance->nifs, -1);	//-1表示永远等待侦听
+		//pool返回实际发生的事件个数，如果失败，返回-1
+		if (ready < 0) {	
+			perror("Poll failed!");
+			break;
+		}
+		else if (ready == 0)	//没任何事情发生，结束本次循环，等下次轮询
+			continue;
+
+		for (int i = 0; i < instance->nifs; i++) {
+			if (instance->fds[i].revents & POLLIN) {
+				len = recvfrom(instance->fds[i].fd, buf, ETH_FRAME_LEN, 0, \
+						(struct sockaddr*)&addr, &addr_len);
+				if (len <= 0) {
+					log(ERROR, "receive packet error: %s", strerror(errno));
+				}
+				else if (addr.sll_pkttype == PACKET_OUTGOING) {
+					// XXX: Linux raw socket will capture both incoming and
+					// outgoing packets, while we only care about the incoming ones.
+
+					// log(DEBUG, "received packet which is sent from the "
+					// 		"interface itself, drop it.");
+				}
+				else {
+					iface_info_t *iface = fd_to_iface(instance->fds[i].fd);
+					if (!iface) 
+						continue;
+
+					char *packet = malloc(len);
+					if (!packet) {
+						log(ERROR, "malloc failed when receiving packet.");
+						continue;
+					}
+					memcpy(packet, buf, len);	//将接受的数据复制到package中
+					handle_packet(iface, packet, len);	//将package广播到其它主机
+				}
+			}
+		}
+	}
+}
+
+int main(int argc, const char **argv)
+{
+	if (getuid() && geteuid()) {		//EUID表示进程对于文件和资源的访问权限，geteuid()返回有效用户组，作为权限验证
+		printf("Permission denied, should be superuser!\n");
+		exit(1);
+	}
+
+	init_ustack();		
+
+	ustack_run();
+
+	return 0;
+}
